04d2e5b6d72bf927dd24e6a329ee6066cc40e146
joewrotehaikus <78707288+joewrotehaikus@users.noreply.github.com>
2023-09-07 14:41:35 -0400

    In PermissionSchema, allRead and allWrite default to false

--- PermissionSchema.js
+++ PermissionSchema.js
@@ -1,32 +1,32 @@
 import { Schema } from "mongoose";
 
 const PermissionSchema = new Schema({
-    allRead: Boolean,
-    allWrite: Boolean,
-    usersRead: [
-        {
-            type: Schema.Types.ObjectId,
-            ref: "user",
-        },
-    ],
-    usersWrite: [
-        {
-            type: Schema.Types.ObjectId,
-            ref: "user",
-        },
-    ],
-    groupsRead: [
-        {
-            type: Schema.Types.ObjectId,
-            ref: "group",
-        },
-    ],
-    groupsWrite: [
-        {
-            type: Schema.Types.ObjectId,
-            ref: "group",
-        },
-    ],
+  allRead: { type: Boolean, default: false },
+  allWrite: { type: Boolean, default: false },
+  usersRead: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "user",
+    },
+  ],
+  usersWrite: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "user",
+    },
+  ],
+  groupsRead: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "group",
+    },
+  ],
+  groupsWrite: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "group",
+    },
+  ],
 });
 
 export default PermissionSchema;
